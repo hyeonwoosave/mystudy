aa42ee78202e2eae0a21adb44166981367e80d1e
JANU <zxv1297@gmail.com>
2024-01-24 17:59:24 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam05/Main.java
@@ -3,15 +3,15 @@ package algorithm.test.baekjoon.level03.exam05;
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
     int N = sc.nextInt();
+    int count = N / 4;
 
-    while () {
-
-
+    for (int i = 0; i < count; i++) {
+      System.out.print("long ");
     }
+    System.out.println("int");
+
   }
 }
--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,6 +1,12 @@
 package bitcamp.myapp;
 
 import bitcamp.menu.MenuGroup;
+import bitcamp.myapp.dao.AssignmentDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.json.AssignmentDaoImpl;
+import bitcamp.myapp.dao.json.BoardDaoImpl;
+import bitcamp.myapp.dao.json.MemberDaoImpl;
 import bitcamp.myapp.handler.HelpHandler;
 import bitcamp.myapp.handler.assignment.AssignmentAddHandler;
 import bitcamp.myapp.handler.assignment.AssignmentDeleteHandler;
@@ -17,62 +23,67 @@ import bitcamp.myapp.handler.member.MemberDeleteHandler;
 import bitcamp.myapp.handler.member.MemberListHandler;
 import bitcamp.myapp.handler.member.MemberModifyHandler;
 import bitcamp.myapp.handler.member.MemberViewHandler;
-import bitcamp.myapp.vo.Assignment;
-import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.ArrayList;
-import bitcamp.util.LinkedList;
-import bitcamp.util.List;
 import bitcamp.util.Prompt;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
-  public static void main(String[] args) throws Exception {
-    Prompt prompt = new Prompt(System.in);
+  Prompt prompt = new Prompt(System.in);
 
-    List<Board> boardRepository = new LinkedList<>();
-    List<Assignment> assignmentRepository = new LinkedList<>();
-    List<Member> memberRepository = new ArrayList<>();
-    List<Board> greetingRepository = new ArrayList<>();
+  List<Member> memberRepository = new ArrayList<>();
 
-    MenuGroup mainMenu = MenuGroup.getInstance("메인");
+  BoardDao boardDao = new BoardDaoImpl("board.json");
+  BoardDao greetingDao = new BoardDaoImpl("greeting.json");
+  AssignmentDao assignmentDao = new AssignmentDaoImpl("assignment.json");
+  MemberDao memberDao = new MemberDaoImpl("member.json");
+
+  MenuGroup mainMenu;
+
+  App() {
+    prepareMenu();
+  }
+
+  public static void main(String[] args) {
+    new App().run();
+  }
+
+  void prepareMenu() {
+    mainMenu = MenuGroup.getInstance("메인");
 
     MenuGroup assignmentMenu = mainMenu.addGroup("과제");
-    assignmentMenu.addItem("등록", new AssignmentAddHandler(assignmentRepository, prompt));
-    assignmentMenu.addItem("조회", new AssignmentViewHandler(assignmentRepository, prompt));
-    assignmentMenu.addItem("변경", new AssignmentModifyHandler(assignmentRepository, prompt));
-    assignmentMenu.addItem("삭제", new AssignmentDeleteHandler(assignmentRepository, prompt));
-    assignmentMenu.addItem("목록", new AssignmentListHandler(assignmentRepository, prompt));
+    assignmentMenu.addItem("등록", new AssignmentAddHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("조회", new AssignmentViewHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("변경", new AssignmentModifyHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("삭제", new AssignmentDeleteHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("목록", new AssignmentListHandler(assignmentDao, prompt));
 
     MenuGroup boardMenu = mainMenu.addGroup("게시글");
-    boardMenu.addItem("등록", new BoardAddHandler(boardRepository, prompt));
-    boardMenu.addItem("조회", new BoardViewHandler(boardRepository, prompt));
-    boardMenu.addItem("변경", new BoardModifyHandler(boardRepository, prompt));
-    boardMenu.addItem("삭제", new BoardDeleteHandler(boardRepository, prompt));
-    boardMenu.addItem("목록", new BoardListHandler(boardRepository, prompt));
+    boardMenu.addItem("등록", new BoardAddHandler(boardDao, prompt));
+    boardMenu.addItem("조회", new BoardViewHandler(boardDao, prompt));
+    boardMenu.addItem("변경", new BoardModifyHandler(boardDao, prompt));
+    boardMenu.addItem("삭제", new BoardDeleteHandler(boardDao, prompt));
+    boardMenu.addItem("목록", new BoardListHandler(boardDao, prompt));
 
     MenuGroup memberMenu = mainMenu.addGroup("회원");
-    memberMenu.addItem("등록", new MemberAddHandler(memberRepository, prompt));
-    memberMenu.addItem("조회", new MemberViewHandler(memberRepository, prompt));
-    memberMenu.addItem("변경", new MemberModifyHandler(memberRepository, prompt));
-    memberMenu.addItem("삭제", new MemberDeleteHandler(memberRepository, prompt));
-    memberMenu.addItem("목록", new MemberListHandler(memberRepository, prompt));
+    memberMenu.addItem("등록", new MemberAddHandler(memberDao, prompt));
+    memberMenu.addItem("조회", new MemberViewHandler(memberDao, prompt));
+    memberMenu.addItem("변경", new MemberModifyHandler(memberDao, prompt));
+    memberMenu.addItem("삭제", new MemberDeleteHandler(memberDao, prompt));
+    memberMenu.addItem("목록", new MemberListHandler(memberDao, prompt));
 
     MenuGroup greetingMenu = mainMenu.addGroup("가입인사");
-    greetingMenu.addItem("등록", new BoardAddHandler(greetingRepository, prompt));
-    greetingMenu.addItem("조회", new BoardViewHandler(greetingRepository, prompt));
-    greetingMenu.addItem("변경", new BoardModifyHandler(greetingRepository, prompt));
-    greetingMenu.addItem("삭제", new BoardDeleteHandler(greetingRepository, prompt));
-    greetingMenu.addItem("목록", new BoardListHandler(greetingRepository, prompt));
+    greetingMenu.addItem("등록", new BoardAddHandler(greetingDao, prompt));
+    greetingMenu.addItem("조회", new BoardViewHandler(greetingDao, prompt));
+    greetingMenu.addItem("변경", new BoardModifyHandler(greetingDao, prompt));
+    greetingMenu.addItem("삭제", new BoardDeleteHandler(greetingDao, prompt));
+    greetingMenu.addItem("목록", new BoardListHandler(greetingDao, prompt));
 
     mainMenu.addItem("도움말", new HelpHandler(prompt));
+  }
 
-    // 프로그램을 실행하다가 어느 지점에서 예외가 발생하면 해당 위치에서 적절한 조치를 취할 것이다.
-    // 다만 그에 벗어나서 조치가 되지 않은 예외가 보고 되는 경우를 대비해
-    // 마지막 보루인 main()에서는 예외를 처리해야 한다.
-    // main()에서 마저 처리하지 않는다면 JVM에게 보고될 것이고,
-    // JVM은 개발자나 알아 볼 메시지를 출력하고 종료할 것이다.
-    //
+  void run() {
     while (true) {
       try {
         mainMenu.execute(prompt);
