45f5f726fa6c9710e47d491c1712abfcf28aa346
JANU <zxv1297@gmail.com>
2024-02-06 15:44:37 +0900

    .

--- java-basic/app/src/main/java/com/eomcs/jdbc/ex1/Exam0111.java
+++ java-basic/app/src/main/java/com/eomcs/jdbc/ex1/Exam0111.java
@@ -9,12 +9,12 @@ public class Exam0111 {
   public static void main(String[] args) {
 
     try {
-      java.sql.Driver mariadbDriver = new org.mariadb.jdbc.Driver(); 
+      java.sql.Driver mariadbDriver = new org.mariadb.jdbc.Driver();
       DriverManager.registerDriver(mariadbDriver);
       System.out.println("JDBC 드라이버 로딩 및 등록 완료!");
 
       // 등록되지 않은 드라이버를 찾을 경우 예외 발생!
-      java.sql.Driver driver = DriverManager.getDriver("jdbc:db2://");
+      java.sql.Driver driver = DriverManager.getDriver("jdbc:mysql://");
       System.out.println(driver);
 
     } catch (SQLException e) {
